10a6eb9212847ec1b97a050bc0dccb465fa43fff
Matthew Moczynski <103980603+mmoczynski@users.noreply.github.com>
2024-07-02 15:01:54 -0500

    Adding support for more objects, removing preview image that is dragged

--- html5/src/generateDefinitionsHTML.js
+++ html5/src/generateDefinitionsHTML.js
@@ -1,17 +1,34 @@
 import definitionTree from "./definitionTree.js";
 import creator from "./creator.js";
 import { GameLevel, GameObject } from "./gameLevel.js";
+import Ghost from "./ghost.js";
 
 function createMenuItem(definition) {
 
     var elm = document.createElement("div");
     elm.classList.add("object");
 
+    var img = new Image();
+
+    img.onerror = function() {
+        this._broken = true;
+    }
+
+    img.src = "images/sprites/" + definition['@_cid'] + ".svg";
+    definition.svgSprite = img;
+
+    img.addEventListener("load", function(){
+        definition.gridDividesX = (img.width % creator.gridSize === 0);
+        definition.gridDividesY = (img.height % creator.gridSize === 0)
+    })
+
+    img.draggable = false;
+
+    elm.appendChild(img);
+
     var span = document.createElement("span");
     span.innerText = definition["@_cname"];
     elm.appendChild(span);
-
-    elm._creator_dictionary_entry = definition;
 
     return elm;
 }
@@ -34,41 +51,67 @@ function generateDefintionsHTML() {
 
     for(let i = 0; i < definitionTree.categories.block.length; i++) {
 
-        var elm = createMenuItem(definitionTree.categories.block[i]);
+        let elm = createMenuItem(definitionTree.categories.block[i]);
+
+        elm._creator_dictionary_entry = definitionTree.categories.block[i];
 
         elm.addEventListener("mousedown", function(){
 
-            var newElm = document.createElement("img");
-            newElm.style.position = "absolute";
-            newElm.src = "799.svg";
-            newElm.style.zIndex = 300;
+            //var newElm = document.createElement("img");
+            //newElm.style.position = "absolute";
+            //newElm.src = elm._creator_dictionary_entry.svgSprite.src;
+            //newElm.style.zIndex = 300;
+            //newElm.draggable = false;
 
-            document.body.appendChild(newElm);
+            // Game object
+
+            var newGameObject = new GameObject(
+                elm._creator_dictionary_entry['@_cid'],
+                creator.mousePosition.world.x,
+                creator.mousePosition.world.y
+            );
+
+            newGameObject.ghost = new Ghost(newGameObject);
+
+            if(creator.gameInstance.level instanceof GameLevel) {
+                creator.gameInstance.level.objects.push(newGameObject);
+            }
+
+            //newElm.width = Math.floor(newElm.width * creator.zoomFactor);
+            //newElm.height = Math.floor(newElm.height * creator.zoomFactor);
+
+            //document.body.appendChild(newElm);
 
             var move = function(event){
-                newElm.style.left = event.x + "px";
-                newElm.style.top = event.y + "px";
+
+                //let x = (event.x - newElm.width * creator.zoomFactor * 0.5);
+                //let y = (event.y - newElm.height * creator.zoomFactor * 0.5);
+
+                newGameObject.x = creator.mousePosition.world.x;
+                newGameObject.y = creator.mousePosition.world.y;
+
+                newGameObject.ghost.updatePoints();
+
+                //console.log(creator.mousePosition.world);
+
+                //console.log(newGameObject);
+
+                //newElm.style.left = x + "px";
+                //newElm.style.top = y + "px";
             }
 
             var disable = function() {
-                document.body.removeEventListener("mousemove", move);
-                document.body.removeEventListener("mouseup", disable);
-                document.body.removeChild(newElm);
+                window.removeEventListener("mousemove", move);
+                window.removeEventListener("mouseup", disable);
+                //document.body.removeChild(newElm);
 
-                if(creator.gameInstance.level instanceof GameLevel) {
+                newGameObject.x = newGameObject.ghost.centerPoint.x;
+                newGameObject.y = newGameObject.ghost.centerPoint.y;
 
-                    creator.gameInstance.level.objects.push(new GameObject(
-                        elm._creator_dictionary_entry['@_cid'],
-                        creator.mousePosition.gridCell.center.x,
-                        creator.mousePosition.gridCell.center.y
-                    ))
-
-                }
-                
             }
 
-            document.body.addEventListener("mousemove", move);
-            document.body.addEventListener("mouseup", disable);
+            window.addEventListener("mousemove", move);
+            window.addEventListener("mouseup", disable);
 
         });
 
@@ -98,7 +141,7 @@ function generateDefintionsHTML() {
         document.querySelector("#elements").innerHTML = "";
         document.querySelector("#elements").appendChild(o.blocks_and_tiles);
         changeSelectionClass(this);
-        o.blocks_and_tiles.scrollTo(0,-temp1.scrollTop);
+        o.blocks_and_tiles.scrollTo(0,this.scrollTop);
     });
 
     document.querySelector(".obj-menu-item.walls-and-decoration").addEventListener("click", function(){
