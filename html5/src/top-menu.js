c03c71cb34b2e7a6479853cb99397fa859d766d0
Matthew Moczynski <103980603+mmoczynski@users.noreply.github.com>
2024-07-09 09:45:57 -0500

    Moving code to proper file

--- html5/src/top-menu.js
+++ html5/src/top-menu.js
@@ -1,10 +1,10 @@
+import creator from "./creator.js";
+import Modal from "./modal.js";
+
 /**
  * Options for top menu
  */
 
-document.querySelector("#new-game-menu-item").addEventListener("click", function(){
-
-});
 
 document.querySelector("#load-game-menu-item").addEventListener("click", function(){
 
@@ -18,10 +18,32 @@ document.querySelector("#save-game-as-menu-item").addEventListener("click", func
 
 });
 
-document.querySelector("#test-game-menu-item").addEventListener("click", function(){
+document.querySelector("#new-game-menu-item").addEventListener("click", function(){
+    Modal.confirm("Creating a new game will erase any unsaved game you are working on. Do you want to do this?")
+});
 
+document.querySelector("#test-game-menu-item").addEventListener("click", function(){
+    Modal.alert("Testing games is not available right now.");
 });
 
 document.querySelector("#publish-game-menu-item").addEventListener("click", function(){
+    Modal.alert("Publishing games is not available right now.");
+});
+
+document.querySelector("#rename-level-button").addEventListener("click", function(){
+
+    Modal.prompt(
+
+        "Enter in new name for level:", 
+
+        function(input) {
+            creator.gameInstance.level.name = input;
+        }, 
+
+        function() {}, 
+
+        creator.gameInstance.level.name
+
+    );
 
 });
