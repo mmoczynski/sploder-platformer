b651acba57a3b2c34eeafe715d2037b490108de9
Matthew Moczynski <103980603+mmoczynski@users.noreply.github.com>
2024-07-18 23:02:45 -0500

    Moving code.

--- html5/src/top-menu.js
+++ html5/src/top-menu.js
@@ -5,31 +5,6 @@ import Modal from "./modal.js";
  * Options for top menu
  */
 
-
-document.querySelector("#load-game-menu-item").addEventListener("click", function(){
-
-});
-
-document.querySelector("#save-game-menu-item").addEventListener("click", function(){
-
-});
-
-document.querySelector("#save-game-as-menu-item").addEventListener("click", function(){
-
-});
-
-document.querySelector("#new-game-menu-item").addEventListener("click", function(){
-    Modal.confirm("Creating a new game will erase any unsaved game you are working on. Do you want to do this?")
-});
-
-document.querySelector("#test-game-menu-item").addEventListener("click", function(){
-    Modal.alert("Testing games is not available right now.");
-});
-
-document.querySelector("#publish-game-menu-item").addEventListener("click", function(){
-    Modal.alert("Publishing games is not available right now.");
-});
-
 document.querySelector("#rename-level-button").addEventListener("click", function(){
 
     Modal.prompt(
